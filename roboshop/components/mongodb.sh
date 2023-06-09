5896e685cd046befe1ecbdf57d0b12b5819ad58d
Sri <srimathiselvaraj@gmaicom>
2023-06-09 17:16:47 +0530

    Auto push

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,3 +1,51 @@
 #!/bin/bash
 
-echo "I am mongodb"
+COMPONENT=mongodb
+LOGFILE="/tmp/${COMPONENT}.log"
+
+ID=$(id -u)
+if [ $ID -ne 0 ] ; then
+    echo "\e[31m This script is expectd to be run by root user or with sudo previlage \e[0m"
+    exit 1
+fi
+
+stat(){
+if [ $1 -eq 0 ] ; then
+    echo -e "\e[32m success \e[0m"
+else
+    echo -e "\e[32m failure \e[0m"
+    exit 2
+fi   
+}
+
+echo -n "Configuring the $COMPONENT repo: "
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
+stat $?
+
+##Setup MongoDB repos.
+##echo '[mongodb-org-4.2]
+##name=MongoDB Repository
+##baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
+##gpgcheck=1
+##enabled=1
+##gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
+##Install Mongo & Start Service.
+# yum install -y mongodb-org 
+# systemctl enable mongod
+# systemctl start mongod
+Update Liste IP address from 127.0.0.1 to 0.0.0.0 in config file
+Config file: /etc/mongod.conf
+
+##then restart the service
+
+# systemctl restart mongod
+##Every Database needs the schema to be loaded for the application to work.
+##Download the schema and load it.
+
+# curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
+
+# cd /tmp
+# unzip mongodb.zip
+# cd mongodb-main
+# mongo < catalogue.js
+# mongo < users.js 
