60b78dd29fdea5a7dcea83767b8c20c078286d71
Sri <srimathiselvaraj@gmaicom>
2023-06-09 19:19:03 +0530

    Auto push

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -32,7 +32,7 @@ systemctl start mongod     &>> $LOGFILE
 stat $?
 
 echo -n " Enabling the DB visibility"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongo.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 stat $?
 
 echo -n "starting  $COMPONENT service: "
