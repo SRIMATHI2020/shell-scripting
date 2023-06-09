b419da0b5e4aab0dd0f9f6809727bf8a92559060
Sri <srimathiselvaraj@gmaicom>
2023-06-09 19:27:24 +0530

    Auto push

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -33,38 +33,20 @@ stat $?
 echo -n "starting $COMPONENT :"
 systemctl daemon-reload mongod   &>> $LOGFILE
 systemctl enable mongod          &>> $LOGFILE
-systemctl restart mongod           &>> $LOGFILE
+systemctl restart mongod         &>> $LOGFILE
 stat $?
 
+echo -n "Download the $COMPONENT schema: "
+curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
+stat $?
 
+echo -n "Extracting the $COMPONENT schema: "
+cd /tmp
+unzip mongodb.zip &>> $LOGFILE
+stat $?
 
-
-
-
-
-##Setup MongoDB repos.
-##echo '[mongodb-org-4.2]
-##name=MongoDB Repository
-##baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
-##gpgcheck=1
-##enabled=1
-##gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
-##Install Mongo & Start Service.
-# systemctl enable mongod
-# systemctl start mongod
-Update Liste IP address from 127.0.0.1 to 0.0.0.0 in config file
-Config file: /etc/mongod.conf
-
-##then restart the service
-
-# systemctl restart mongod
-##Every Database needs the schema to be loaded for the application to work.
-##Download the schema and load it.
-
-# curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
-
-# cd /tmp
-# unzip mongodb.zip
-# cd mongodb-main
-# mongo < catalogue.js
-# mongo < users.js 
+echo -n "Injecting the $COMPONENT schema: "
+cd $COMPONENT-main
+mongo < catalogue.js
+mongo < users.js 
+stat $?
